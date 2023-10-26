62c17ea8b877935197113a8670521acb24f32de8
José Victor <josetvictor@pop-os.localdomain>
2023-10-26 11:18:04 -0300

    Adicionando configuração de autenticação no swagger

--- src/ApiCatalogo/Program.cs
+++ src/ApiCatalogo/Program.cs
@@ -6,6 +6,7 @@ using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.IdentityModel.Tokens;
+using Microsoft.OpenApi.Models;
 using System.Text;
 using System.Text.Json.Serialization;
 
@@ -13,9 +14,39 @@ using System.Text.Json.Serialization;
 var builder = WebApplication.CreateBuilder(args);
 
 // ignorando objetos quando um ciclo de referência é detectado durante a serialização
-builder.Services.AddControllers().AddJsonOptions(op => op.JsonSerializerOptions.ReferenceHandler = ReferenceHandler.IgnoreCycles);
+builder.Services.AddControllers()
+                .AddJsonOptions(op => 
+                    op.JsonSerializerOptions.ReferenceHandler = ReferenceHandler.IgnoreCycles);
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(c => {
+    c.SwaggerDoc("v1", new OpenApiInfo { Title =  "APICatalogo", Version = "v1"});
+
+    c.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme()
+    {
+        Name = "Autorization",
+        Type = SecuritySchemeType.ApiKey,
+        Scheme = "Bearer",
+        BearerFormat = "JWT",
+        In = ParameterLocation.Header,
+        Description = @"Header de autorização JWT usando o esquema Bearer.\r\n\r\nInformate
+        'Bearer'[espaço] e o seu token.\r\n\r\nExamplo:\'Bearer 12345abcdef\'",
+    });
+
+    c.AddSecurityRequirement(new OpenApiSecurityRequirement
+    {
+        {
+            new OpenApiSecurityScheme
+            {
+                Reference = new OpenApiReference
+                {
+                    Type = ReferenceType.SecurityScheme,
+                    Id = "Bearer"
+                }
+            },
+            Array.Empty<string>()
+        }
+    });
+});
 
 var mappingConfig = new MapperConfiguration(mc => {
     mc.AddProfile(new MappingProfile());
